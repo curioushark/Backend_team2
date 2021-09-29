203dd7afe3c3025a8b0483de28f1b2a2a9b99716
Ghina D <90589117+curioushark@users.noreply.github.com>
2021-09-29 22:49:12 +0700

    Update AccountsController.java

--- AccountsController.java
+++ AccountsController.java
@@ -1,46 +1,46 @@
-package com.jdt8.bank.controller;
+package com.ghina.Bank_Team2.controller;
 
-
-import com.jdt8.bank.entity.Accounts;
-import com.jdt8.bank.entity.temp.AccCust;
-import com.jdt8.bank.service.AccountsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ghina.Bank_Team2.entity.temp.AccCust;
+import com.ghina.Bank_Team2.entity.temp.TransInfo;
+import com.ghina.Bank_Team2.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/accounts")
-public class AccountsController {
+import java.util.List;
 
-    @Autowired
-    private AccountsService accountsService;
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+    
+    private AccountService accountService;
 
     @PostMapping("/save")
-    public ResponseEntity<Object> createAccount(@RequestBody AccCust data){
-        Object accounts = accountsService.createAccount(data);
-        if(accounts.equals(false)){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
+    public ResponseEntity<Object>createAccount(@RequestBody AccCust data){
+        Object account = accountService.createAccount(data);
+        if (account.equals(false)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(accounts,HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
     }
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody AccCust data){
-        Object send = accountsService.login(data);
-        if(send.equals(false)){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
+    public ResponseEntity<Object>login(@RequestBody AccCust data){
+        Object send= accountService.createAccount(data);
+        if (send.equals(false)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(send,HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-    @GetMapping("/find/{id}")
-    public ResponseEntity<Object> findCustomer(@PathVariable("id")String id){
-        Object x = accountsService.findByAccountId(id);
-        if(x.equals(false)){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
+    @GetMapping("/info/{id}")
+    public ResponseEntity<Object>findCustomer(@PathVariable("id") String id){
+        Object x = accountService.balanceInfo(id);
+        if (x.equals(false)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(x,HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
+    @GetMapping("/findtrans/{id}")
+    public ResponseEntity<List<TransInfo>>findTrans(@PathVariable("id") String id){
+        return new ResponseEntity<>(accountService.findTransaction(id),HttpStatus.OK);
+    }
 }
