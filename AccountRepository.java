202adff4e27d61cfea1b08558a0dece427df8366
Ghina D <90589117+curioushark@users.noreply.github.com>
2021-09-29 22:47:51 +0700

    Update AccountRepository.java

--- AccountRepository.java
+++ AccountRepository.java
@@ -1,12 +1,14 @@
-package com.jdt8.bank.repository;
+package com.ghina.Bank_Team2.repository;
 
-import com.jdt8.bank.entity.Accounts;
-import com.jdt8.bank.entity.Customer;
+import com.ghina.Bank_Team2.entity.Account;
+import com.ghina.Bank_Team2.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface AccountRepository extends JpaRepository<Accounts,String> {
+public interface AccountRepo extends JpaRepository<Account, String> {
+    List<Account>findByCustomer(Customer customer);
 
-    List<Accounts> findByCustomer(Customer customer);
+    Optional<Account> findByAccNumber(int accNumber);
 }
